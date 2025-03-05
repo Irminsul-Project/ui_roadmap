b5733c74a2888fa0543f09b6d71e2fd287abb322
jimmy ready <jimmyready>
2025-03-05 20:37:14 +0700

    fix: adjust style

--- src/page/timeline-home.vue
+++ src/page/timeline-home.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import hyperlink from '../components/button/hyperlink.vue';
 import DataView from 'primevue/dataview';
+import Card from 'primevue/card';
 
 const ReasearchListData = ref([
     {
@@ -10,7 +11,7 @@ const ReasearchListData = ref([
         "Id" : "PHP"
     },
     {
-        "Title" : "Php List",
+        "Title" : "Php Budi",
         "LastUpadte" : "20-Jan-2024 20:10",
         "Id" : "PHP1"
     }
@@ -19,7 +20,7 @@ const ReasearchListData = ref([
 </script>
 
 <template>
-    <div class="flex">
+    <div class="flex mb-[10px]">
         <div class="flex-1">
             <h1 class="mb-3">
                 Select Reasearch
@@ -29,28 +30,32 @@ const ReasearchListData = ref([
             <hyperlink title="Create New" to="/timelinecreate"/>
         </div>
     </div>
-    <DataView :value="ReasearchListData">
-        <template #list="ReasearchList">
-            <div class="flex flex-col">
-                <div v-for="(Reasearch, index) in ReasearchList.items" :key="Reasearch.Id">
-                    <div class="flex flex-col sm:flex-row sm:items-center p-6 gap-4" :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
-                        <div class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6">
-                            <div class="flex flex-row md:flex-col justify-between items-start gap-2">
-                                <div>
-                                    <div class="text-lg font-medium mt-2">{{ Reasearch.Title }}</div>
-                                </div>
-                            </div>
-                            <div class="flex flex-col md:items-end gap-8">
-                                <div class="flex flex-row-reverse md:flex-row gap-2">
-                                    <hyperlink title="Open" to="/timeline/php"/>
+    <Card>
+        <template #content>
+            <DataView :value="ReasearchListData">
+                <template #list="ReasearchList">
+                    <div class="flex flex-col">
+                        <div v-for="(Reasearch, index) in ReasearchList.items" :key="Reasearch.Id">
+                            <div class="flex flex-col sm:flex-row sm:items-center p-6 gap-4" :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
+                                <div class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6">
+                                    <div class="flex flex-row md:flex-col justify-between items-start gap-2">
+                                        <div>
+                                            <div class="text-lg font-medium mt-2">{{ Reasearch.Title }}</div>
+                                        </div>
+                                    </div>
+                                    <div class="flex flex-col md:items-end gap-8">
+                                        <div class="flex flex-row-reverse md:flex-row gap-2">
+                                            <hyperlink title="Open" to="/timeline/php"/>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </template>
+            </DataView>
         </template>
-    </DataView>
+    </Card>
 </template>
 
 <style scoped>
